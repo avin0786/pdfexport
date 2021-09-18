34a8deaeb2f3b632b298a18caa532dfd1ef90b27
avin0786 <30489901+avin0786@users.noreply.github.com>
2021-09-18 16:08:20 +0530

    initial commmit

--- src/main/java/com/pdf/pdfexport/util/ExportPdfFile.java
+++ src/main/java/com/pdf/pdfexport/util/ExportPdfFile.java
@@ -1,16 +1,23 @@
 package com.pdf.pdfexport.util;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.pdfbox.cos.COSBase;
+import org.apache.pdfbox.io.IOUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
 import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
 import org.apache.pdfbox.pdmodel.interactive.form.PDField;
+import org.apache.pdfbox.pdmodel.interactive.form.PDXFAResource;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.springframework.core.io.ClassPathResource;
+import org.w3c.dom.Document;
 
 public class ExportPdfFile {
 
@@ -26,13 +33,24 @@ public class ExportPdfFile {
 			PDDocument pdDocumnet = PDDocument.load(pdfFile.getFile());
 			PDAcroForm pdAcroForm = pdDocumnet.getDocumentCatalog()
 					.getAcroForm();
-			Map<String,String> dataMap = getPdfFileData();
-			for (Map.Entry<String, String> entry : dataMap.entrySet()) {
+			
+			ClassPathResource pdfFile2 = new ClassPathResource(
+					"pdf/Form_AOC4.pdf");
+			PDDocument pdDocumnet2 = PDDocument.load(pdfFile2.getFile());
+			PDDocumentCatalog docCatalog = pdDocumnet2.getDocumentCatalog();
+		    PDAcroForm form = docCatalog.getAcroForm();
+		    PDXFAResource xfa = form.getXFA();
+	        Document domDocument = xfa.getDocument();
+	        System.out.println(domDocument.getNodeValue());
+			pdAcroForm.setXFA(xfa);
+			//Map<String,String> dataMap = getPdfFileData();
+			/*for (Map.Entry<String, String> entry : dataMap.entrySet()) {
 				if (pdAcroForm.getField((String) entry.getKey()) != null) {
 					pdAcroForm.getField((String) entry.getKey()).setValue(
 							(String) entry.getValue());
 				}
-			}
+			} */
+			pdDocumnet.save(new File("abc.pdf"));
 			return pdfFile.getInputStream();
 
 		} catch (Exception e) {
@@ -49,17 +67,37 @@ public class ExportPdfFile {
 			ClassPathResource pdfFile = new ClassPathResource(
 					"pdf/Form_AOC4.pdf");
 			PDDocument pdDocumnet = PDDocument.load(pdfFile.getFile());
+			PDDocumentCatalog docCatalog = pdDocumnet.getDocumentCatalog();
+		    PDAcroForm form = docCatalog.getAcroForm();
+		    PDXFAResource xfa = form.getXFA();
+		    
+		    String s = new String(xfa.getBytes());
+		    Iterator<PDField> iterate  = form.getFieldIterator();
+		    while(iterate.hasNext())
+		    	
+		    {
+		    	PDField item =iterate.next();
+		    	System.out.println("Item="+item.getFieldType());
+		    	System.out.println("Item="+item.getWidgets());
+		    }
+		   /* for(PDField filed :form.getFields()){
+		    	System.out.println("filed=" +filed.getAlternateFieldName());
+				System.out.println("filed=" +filed.getFullyQualifiedName());
+				dataMap.put(filed.getPartialName(), filed.getValueAsString());
+		    }
+		   
+		    //String replacementString = IOUtils.toString(data.getDataStream());
 			PDAcroForm pdAcroForm = pdDocumnet.getDocumentCatalog()
 					.getAcroForm();
 			List<PDField> fileds = pdAcroForm.getFields();
 			for (PDField filed :fileds) {
-				System.out.println("filed=" +filed.getAlternateFieldName());
-				System.out.println("filed=" +filed.getFullyQualifiedName());
-				System.out.println("PDFTextStripper=" +new PDFTextStripper().getText(pdDocumnet));
+				//System.out.println("filed=" +filed.getAlternateFieldName());
+				//System.out.println("filed=" +filed.getFullyQualifiedName());
+				//System.out.println("PDFTextStripper=" +new PDFTextStripper().getText(pdDocumnet));
 				dataMap.put(filed.getPartialName(), filed.getValueAsString());
 			}
-			System.out.println("Data map=" +dataMap.toString());
-			
+			//System.out.println("Data map=" +dataMap.toString());
+			*/
 
 		} catch (Exception e) {
 			e.printStackTrace();
--- src/main/java/com/pdf/pdfexport/util/MainTest.java
+++ src/main/java/com/pdf/pdfexport/util/MainTest.java
@@ -7,6 +7,20 @@ public class MainTest {
 	public static void main(String[] args){
 		 System.out.println("I am a test");
 		 try {
+			 PdfDocument pdf = new PdfDocument(new PdfReader(SRC));
+			    PdfAcroForm form = PdfAcroForm.getAcroForm(pdf, true);
+			    XfaForm xfa = form.getXfaForm();
+			    Document doc = xfa.getDomDocument();
+			    DOMSource domSource = new DOMSource(doc);
+			    StringWriter writer = new StringWriter();
+			    StreamResult result = new StreamResult(writer);
+			    TransformerFactory tf = TransformerFactory.newInstance();
+			    Transformer transformer = tf.newTransformer();
+			    transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+			    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			    transformer.transform(domSource, result);
+			    writer.flush();
+			    System.out.println(writer.toString());
 			ExportPdfFile.getPdfFile();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
